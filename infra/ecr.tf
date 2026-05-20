b7ebe6ffbd105c28101d70636e496981b31de336
Hector Avila <hectoravllr@gmail.com>
2026-05-20 11:51:31 -0300

    chore(infra): enable force destroy on ECR repository
    - Added 'force_delete = true' to the aws_ecr_repository resource to ensure 'terraform destroy' can tear down the repository cleanly even when it contains Docker images, preventing dependency and active resource blocks.

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "gitops_ecr_repository" {
   name                 = "gitops-ecr-repository"
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
